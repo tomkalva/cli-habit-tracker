78766e11222efae243cd7c7a1eef43e7133eec30
tomkalva <bvanflez@gmail.com>
2026-05-14 12:56:12 +0300

    Creates a sql table, can add and list habits

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
+import sqlite3
+
+
 
 parser = argparse.ArgumentParser()
 
 subparsers = parser.add_subparsers(dest="command")
+
 add_parser = subparsers.add_parser("add")
 add_parser.add_argument("habit")
 
@@ -11,8 +15,38 @@ list_parser = subparsers.add_parser("list")
 args = parser.parse_args()
 
 
-if args.command == "add":
-    print(f"Adding {args.habit}")
 
+connection = sqlite3.connect("habits.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS habits (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+)
+""")
+
+
+
+
+if args.command == "add":
+    print(f"Added {args.habit}")
+
+    cursor.execute(
+    "INSERT INTO habits (name) VALUES (?)",
+    (args.habit,)
+    )
+    
 if args.command == "list":
-    print("list")    
+    cursor.execute("SELECT * FROM habits")
+    rows = cursor.fetchall()
+
+    print_string = ""
+    for habit in rows:
+        print_string += f"{habit[0]}.{habit[1]}\n"
+
+    print(print_string)
+
+
+connection.commit()
+connection.close()
